cf6d50b7762ed81c2e2d4fea21e2ed8cd3c9cc7f
Edem <ziddah.edem@gmail.com>
2021-06-01 14:35:48 +0000

    :tada: fixing bugs

--- actions/Pagination.js
+++ actions/Pagination.js
@@ -1,27 +1,42 @@
 import { Spinner } from "react-bootstrap";
-import { useEffect } from "react";
 import { useSWRPages } from "swr";
 import { useGetBlogs } from "actions";
 import CardListItem from "components/CartListItem";
 import CardsItemRow from "components/CardsItemRow";
 
-export const useGetBlogPages = ({ blogs, filter }) => {
-  useEffect(() => {
-    window.__pagination__init = true;
-  }, []);
+const BlogList = ({ blogs, filter }) => {
+  return filter.view.list ? (
+    blogs.map((blog) => (
+      <div key={`${blogs.length}-${Math.random()}-list`}>
+        <CardListItem
+          title={blog.title}
+          coverImage={blog.coverImage}
+          subtitle={blog.subtitle}
+          date={blog.date}
+          author={blog.author}
+          link={{
+            href: "blogs/[slug]",
+            as: `blogs/${blog.slug}`,
+          }}
+        />
+      </div>
+    ))
+  ) : (
+    <CardsItemRow paginatedBlogs={blogs} />
+  );
+};
 
+export const useGetBlogPages = ({ blogs, filter }) => {
   return useSWRPages(
     "index-page",
     ({ offset, withSWR }) => {
-      let initialData = !offset && blogs;
-
-      if (typeof window !== "undefined" && window.__pagination__init) {
-        initialData = null;
-      }
-
-      const { data: paginatedBlogs } = withSWR(
-        useGetBlogs({ offset, filter }, initialData)
+      const { data: paginatedBlogs, error } = withSWR(
+        useGetBlogs({ offset, filter })
       );
+
+      if (!offset && !paginatedBlogs && !error) {
+        return <BlogList blogs={blogs} filter={filter} />;
+      }
 
       if (!paginatedBlogs) {
         return (
@@ -31,25 +46,8 @@ export const useGetBlogPages = ({ blogs, filter }) => {
         );
       }
 
-      return filter.view.list ? (
-        paginatedBlogs.map((blog) => (
-          <div key={`${blogs.length}-${Math.random()}-list`}>
-            <CardListItem
-              title={blog.title}
-              coverImage={blog.coverImage}
-              subtitle={blog.subtitle}
-              date={blog.date}
-              author={blog.author}
-              link={{
-                href: "blogs/[slug]",
-                as: `blogs/${blog.slug}`,
-              }}
-            />
-          </div>
-        ))
-      ) : (
-        <CardsItemRow paginatedBlogs={paginatedBlogs} />
-      );
+      return <BlogList blogs={paginatedBlogs} filter={filter} />;
+      // return blog list
     },
 
     (SWR, index) => {
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -8,7 +8,7 @@ export default async function enablePreview(req, res) {
     return res.status(401).json({ message: "Invalid Token" });
   }
 
-  const blog = await getBlogBySlug(req.query.slug);
+  const blog = await getBlogBySlug(req.query.slug, true);
 
   if (!blog) {
     return res.status(401).json({ message: "Invalid Slug" });
