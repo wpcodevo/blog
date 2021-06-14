6ac5349ff2dda1ecc777749fbc23aa3ed0fe1887
Edem <ziddah.edem@gmail.com>
2021-06-14 19:03:33 +0000

    fixing bugs

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import { Image } from "react-bootstrap";
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <div className='error404'>
+      <div className='errorImage'>
+        <Image src='/images/404.svg' height='100%' />
+      </div>
+      <div className='content'>
+        <p>The Page You are looking for is now beyound our Reach...</p>
+        <Link href='/'>
+          <a>Go Home</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ pages/500.js
@@ -0,0 +1,22 @@
+import { Image } from "react-bootstrap";
+import Link from "next/link";
+
+export default function Custom500() {
+  return (
+    <div className='error404'>
+      <div className='errorImage'>
+        <Image src='/images/500.svg' height='100%' />
+      </div>
+      <div className='content'>
+        <h3>Oops, something went wrong</h3>
+        <p>
+          Server error 500. We apologise and are fixing the problem. Try again
+          at a later time.
+        </p>
+        <Link href='/'>
+          <a>Bact to Home</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
