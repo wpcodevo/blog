bbff79358222c749f6c5c2eb71b6fa7438d4e04f
Edem <ziddah.edem@gmail.com>
2021-07-01 14:47:00 +0000

    adding ads

--- components/Aside.js
+++ components/Aside.js
@@ -79,8 +79,8 @@ const Aside = () => {
       <div className='widget advertisement'>
         <GoogleAds format='auto' slot={process.env.VERTICAL_SLOT} />
       </div>
-      <div className='widget' style={{ maxHeight: "60rem !important" }}>
-        <GoogleAds slot={process.env.VERTICAL_SLOT} />
+      <div className='widget advertisement'>
+        <GoogleAds format='auto' slot={process.env.VERTICAL_SLOT} />
       </div>
     </aside>
   );
