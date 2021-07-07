4022d9a2e26a1d21429478c1c918020ca5c70dc9
Edem <ziddah.edem@gmail.com>
2021-07-07 07:03:03 +0000

    removing ads after breadcrump

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -94,9 +94,6 @@ function BlogDetails({ blog: initialBlog, blogTags, preview }) {
               : title.replace(/-/g, " ")
           }
         />
-        <div style={{ margin: "1rem 0 1rem" }}>
-          <GoogleAds slot={process.env.HORIZONTAL_SLOT} />
-        </div>
 
         <Card className={`fj-card fj-card-list card-wrapper card-wrapper-1`}>
           <div className='card-body-wrapper card-body-wrapper-1'>
