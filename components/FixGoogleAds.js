74d33d45d72de9ad2edd4543a59f8db2a73b050b
Edem <ziddah.edem@gmail.com>
2021-08-23 20:13:32 +0000

    improved google horizontal ads

--- components/FixGoogleAds.js
+++ components/FixGoogleAds.js
@@ -2,14 +2,11 @@ import AdSense from "react-adsense";
 
 const FixGoogleAds = () => {
   return (
-    <div
-      className='horizontal'
-      style={{ margin: "1rem 0 1rem", width: "100%", textAlign: "center" }}
-    >
+    <div className='horizontal' style={{ margin: "1rem 0 1rem" }}>
       <AdSense.Google
         client={process.env.NEXT_PUBLIC_DATA_AD_CLIENT}
         slot={process.env.NEXT_PUBLIC_DISPLAY_ADS}
-        style={{ display: "block", height: 200, width: "100%" }}
+        style={{ display: "block", height: 200 }}
         format=''
         layout=''
       />
